3289ef39becc86b5d3ee18590acb29e9248f867f
Dyno <0xDyno@gmail.com>
2023-01-07 19:40:39 +0700

    Continue learning Django

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.intro, name="home"),
     path("chat/", views.chat_gpt_page, name="chat"),
     path("generate/", views.generate_page, name="generate"),
+    path("magic/", views.magics_page, name="magic"),
+    path("magic/<int:pk>", views.magics_item_page, name="magic_item"),
 ]
--- app/views.py
+++ app/views.py
@@ -2,14 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 
+context = {
+    "title": "AI Interface",
+}
+
 
 def intro(request):
-    return render(request=request, template_name="home.html")
+    return render(request=request, template_name="home.html", context=context)
 
 
 def chat_gpt_page(request):
-    return render(request=request, template_name="chat_gpt.html")
+    context_gpt = context.copy()
+    if request.method == "POST":
+        context_gpt["answer"] = "something"
+        
+    return render(request=request, template_name="chat_gpt.html", context=context_gpt)
 
 
 def generate_page(request):
-    return render(request=request, template_name="generate.html")
+    return render(request=request, template_name="generate.html", context=context)
+
+
+def magics_page(request):
+    return render(request=request, template_name="magic.html", context=context)
+
+
+def magics_item_page(request, pk):
+    if pk > 100:
+        mip_context = {"message": "404. Not Found"}
+    else:
+        mip_context = {"message": "Magic things happens here.."}
+        
+    mip_context.update(context)
+    return render(request=request, template_name="magic_item.html", context=mip_context)
